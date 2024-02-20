e383899dcb6e710cf8861a78d3b05e7ed617d12a
alPumpur <aleftinapumpur@gmail.com>
2024-02-21 02:24:20 +0700

    оформление корзины

--- src/views/BasketPage.vue
+++ src/views/BasketPage.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="cart">
-    <div v-for="item in cartItems" :key="item.id">
-      <p>{{ item.name }}</p>
-      <p>Цена: {{ item.price }}</p>
-      <p>Количество: {{ item.quantity }}</p>
-      <button @click="increaseQuantity(item.id)">+</button>
-      <button @click="decreaseQuantity(item.id)">-</button>
-      <button @click="removeFromCart(item.id)">Удалить</button>
+    <div v-for="item in cartItems" :key="item.id" class="order-card">
+      <img class="product_image" :src="item.image" :alt="item.name">
+      <div class="order-details">
+        <p>{{ item.name }}</p>
+        <p>Цена: {{ item.price }}</p>
+        <p>Количество: {{ item.quantity }}</p>
+      </div>
+      <div class="buttons">
+        <button @click="increaseQuantity(item.id)">+</button>
+        <button @click="decreaseQuantity(item.id)">-</button>
+        <button @click="removeFromCart(item.id)">Удалить</button>
+      </div>
     </div>
-    <p>Общая стоимость: {{ total }}</p>
-    <button @click="checkout">Оформить заказ</button>
-    <router-link to="/">Назад</router-link>
+    <div class="footer">
+      <p class="total">Общая стоимость: {{ total }}</p>
+      <button @click="checkout" class="checkout-btn">Оформить заказ</button>
+      <router-link to="/" class="back-link">Назад</router-link>
+    </div>
   </div>
 </template>
 
@@ -46,5 +53,81 @@ const checkout = () => {
 </script>
 
 <style scoped>
-/* Стили для корзины */
+.cart {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+}
+
+.order-card {
+  background-color: #f9f9f9;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  width: 500px;
+}
+
+.product_image {
+  max-width: 150px;
+}
+
+.order-details {
+  flex-grow: 1;
+}
+
+.buttons button {
+  padding: 5px 10px;
+  border: none;
+  background-color: #A0522D;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.buttons button:hover {
+  background-color: #A0522D;
+}
+
+.footer {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.total {
+  font-weight: bold;
+}
+
+.checkout-btn {
+  padding: 10px 20px;
+  background-color: #A0522D;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  height: 40px;
+}
+
+.checkout-btn:hover {
+  background-color: #A0522D;
+}
+
+.back-link {
+  color: #A0522D;
+  text-decoration: none;
+}
+
+.back-link:hover {
+  text-decoration: underline;
+}
 </style>
